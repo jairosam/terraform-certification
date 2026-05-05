d2a9b737064d545abf858595858d41b6eb158b05
jairo <jairobuitrago3@gmail.com>
2026-05-04 22:18:04 -0500

    change visibility parameter

--- basic_blocks/github.tf
+++ basic_blocks/github.tf
@@ -3,5 +3,5 @@ provider "github" {}
 resource "github_repository" "test-github-repo" {
   name        = "test-github-repo"
   description = "A test repository created by Terraform"
-  private     = false
+  visibility  = "public"
 }
