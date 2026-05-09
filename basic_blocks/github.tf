a43ca6b396996a1768bbd9d9341d574f7740a6fc
jairo <jairobuitrago3@gmail.com>
2026-05-08 21:19:25 -0500

    add structure files

--- basic_blocks/github.tf
+++ basic_blocks/github.tf
@@ -1,7 +1,7 @@
 provider "github" {}
 
-#resource "github_repository" "test-github-repo" {
-#  name        = "test-github-repo"
-#  description = "A test repository created by Terraform"
-#  visibility  = "public"
-#}
+resource "github_repository" "test-github-repo" {
+  name        = "test-github-repo"
+  description = "A test repository created by Terraform"
+  visibility  = "public"
+}
--- basic_blocks/random.tf
+++ basic_blocks/random.tf
@@ -1,10 +1,24 @@
 provider "random" {}
 
+variable "number_of_pets" {
+  type = number
+  description = "number of pets"
+}
+
 resource "random_integer" "random_number" {
   min = 1
-  max = 100
+  max = 90
+}
+
+resource "random_pet" "pet_name" {
+  length    = var.number_of_pets
+  separator = "-"
 }
 
 output "random_number" {
   value = random_integer.random_number.result
+}
+
+output "pet_name" {
+  value = random_pet.pet_name.id
 }
