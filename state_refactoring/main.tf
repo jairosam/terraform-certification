4b0f5d7b2e88277043e632ea754578a58e2fa041
jairo <jairobuitrago3@gmail.com>
2026-05-18 12:02:29 -0500

    adding removed block

--- state_refactoring/main.tf
+++ state_refactoring/main.tf
@@ -21,3 +21,31 @@ resource "azurerm_virtual_network" "vnet_2" {
 	resource_group_name = azurerm_resource_group.rg_1.name
 	location = azurerm_resource_group.rg_1.location
 }
+
+resource "azurerm_subnet" "subnet_1" {
+	name = "subnet-1"
+	virtual_network_name = azurerm_virtual_network.vnet_test_1.name
+	resource_group_name = azurerm_resource_group.rg_1.name
+	address_prefixes = [ "10.10.1.0/24" ]
+}
+
+resource "azurerm_subnet" "subnet_2" {
+	name = "subnet-2"
+	virtual_network_name = azurerm_virtual_network.vnet_2.name
+	resource_group_name = azurerm_resource_group.rg_1.name
+	address_prefixes = [ "10.20.1.0/24" ]
+}
+
+removed {
+	from = azurerm_subnet.subnet_1
+	lifecycle {
+		destroy = false
+	}
+}
+
+resource "azurerm_subnet" "subnet_3" {
+	name = "subnet-3"
+	virtual_network_name = azurerm_virtual_network.vnet_test_1.name
+	resource_group_name = azurerm_resource_group.rg_1.name
+	address_prefixes = [ "10.10.2.0/24" ]
+}
